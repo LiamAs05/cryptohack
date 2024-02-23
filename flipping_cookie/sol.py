2d4f095214eb9ccf6ecbface4771c30e5215e0db
LiamAs05 <liamdrop205@gmail.com>
2024-02-23 18:41:42 +0200

    added flipping cookie python

--- flipping_cookie/sol.py
+++ flipping_cookie/sol.py
@@ -1,49 +1,16 @@
-from datetime import datetime, timedelta
-from Crypto.Cipher import AES
-import os
-from Crypto.Util.Padding import pad, unpad
+from requests import get
 
-
-def get_blocks(ciphertext: str) -> list:
-    """_summary_
-    Splits ciphertext into 16 byte blocks
-
-    Args:
-        ciphertext (str): from `get_ciphertext`
-
-    Returns:
-        list: list of ciphertext blocks, each of len 32 (16 bytes)
-    """
-    return [ciphertext[i : i + 32] for i in range(0, len(ciphertext), 32)]
+# Everything is in the writeup
 
 
 def main():
-    KEY = os.urandom(16)
-    # expires_at = (datetime.today() + timedelta(days=1)).strftime("%s")
-    # mock_cookie = f"admin=False;expiry={expires_at}".encode()
-    # cookie_with_iv = "cd636213f6f4b38e900b5a332bccf396fdc4994cca567159590e7ac3a1c80991ea3e185905d9f9d7d86709556b8054f1"
-    # iv, b1, b2 = get_blocks(cookie_with_iv)
-    # admin_equals = (b1[:12], iv)
-
-    # print("IV:", iv)
-    # print("b1:", b1, "p1:", mock_cookie[:16])
-    # print("b2:", b2, "p1:", mock_cookie[16:32])
-
-    expires_at = (datetime.today() + timedelta(days=1)).strftime("%s")
-    cookie = f"admin=False;expiry={expires_at}".encode()
-
-    iv = b'\x00'*16
-    padded = pad(cookie, 16)
-    print(padded)
-    cipher = AES.new(KEY, AES.MODE_CBC, iv)
-    encrypted = cipher.encrypt(padded)
-    ciphertext = iv.hex() + encrypted.hex()
-
-    cipher = AES.new(KEY, AES.MODE_CBC, iv)
-    decrypted = cipher.decrypt(encrypted)
-    unpadded = unpad(decrypted, 16)
-    print({"cookie": ciphertext})
-    print(unpadded)
+    cookie = "2483c5de09129100db629163c54729a5951485f993a7a42fc548cac569f52ad8"
+    malicious_iv = "edd2553317f66948738e1dc3970a1075"
+    print(
+        get(
+            rf"https://aes.cryptohack.org/flipping_cookie/check_admin/{cookie}/{malicious_iv}"
+        ).json()["flag"]
+    )
 
 
 if __name__ == "__main__":

f0b44e2cc6f2375e0925acee8eda1b25b18ba10a
LiamAs05 <liamdrop205@gmail.com>
2024-03-14 00:06:25 +0200

    I-Sorted

--- static_client.py
+++ static_client.py
@@ -1,8 +1,9 @@
-from utils.utils import PrintingSocket
-from time import sleep
-from json import loads, dumps
 from copy import deepcopy
+from json import dumps, loads
 from os import system
+from time import sleep
+
+from utils.utils import PrintingSocket
 
 
 def part_one():
@@ -42,16 +43,25 @@ def part_two():
         sleep(1.5)
         params, t = s.recv(4096).split(b"Intercepted from Bob: ")
 
-        alice_params = loads(params.decode())
+        alice_params = loads(params.decode())  # Observent MITM between alice and bob
         bob_params, t = t.split(b"Intercepted from Alice:")
         bob_params = loads(bob_params.decode())
         alice_flag, _ = t.split(b"Bob")
         alice_flag = loads(alice_flag.decode())
 
-        to_send = deepcopy(alice_params)
-        to_send["g"] = to_send["A"]
-        to_send["A"] = "0x0"
-        s.send(dumps(to_send).encode())
+        to_send = deepcopy(alice_params)  # Love me some deepcopies
+        to_send["g"] = to_send["A"]  # Actual exploit
+        to_send["A"] = "0x0"  # To shorten message
+
+        # Bob reuses the same b value, and we can edit Alice's params
+        # we replace g with g^a (A, Alice's public key)
+        # and bob will provide us with (g^a)^b
+        # since he used the same b for both exchanges (us and Alice)
+        # he basically gave us his and Alice's shared secret.
+
+        s.send(
+            dumps(to_send).encode()
+        )  # Normal conversation with bob, but actually malicious
         sleep(1)
         t = s.recv(4096).split(b"Bob says to you: ")
         _, bob_params_2, bob_flag_2 = t
